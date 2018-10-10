9f964e248b14ad68a5326bd3c1ffe57293fecc3f
Jonathan Chan <jonchan816@gmail.com>
2018-10-10 07:06:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ function updateObjectWithKeyAndValue (object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
- return obj[key] = value;
+ obj[key] = value;
+ return obj;
 }
 
+function deleteFromObjectByKey(object, key)
